cd26e9b623a752a5b206ec92f3284d463dafc07e
James Thorne <jamesthorne@canva.com>
2024-05-16 17:08:42 +1000

    refactor: git.go to shell.go

--- internal/git/shell.go
+++ internal/git/shell.go
@@ -99,3 +99,15 @@ func GetCommitHash(branch string) string {
 func ResetRemote(remote string, branch string) string {
 	return fmt.Sprintf("git reset --hard %s/%s", remote, branch)
 }
+
+func GitHubPrCreate(parentBranch string) string {
+	return fmt.Sprintf("gh pr create --base %s --web", parentBranch)
+}
+
+func GitHubPrEditBody(number int, body string) string {
+	return fmt.Sprintf("gh pr edit %d --body-file - <<EOF\n%s\nEOF", number, body)
+}
+
+func GitHubPrState() string {
+	return "gh pr status --json state --jq '.currentBranch.state'"
+}
